e7d0a656b72f0337745390d81bdff1f02acf4865
krash3125 <khannakushagr@gmail.com>
2022-01-04 10:49:57 -0800

    responsiveness

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,36 +7,39 @@ import pcq from "../img/pcq.png";
 
 const ProjectCard = (params) => {
   return (
-    <div class="project-card col-md">
-      <a href={params.link} target="_blank" rel="noreferrer">
-        <div
-          className="project-card-img col"
-          style={{
-            background: `url(${params.img})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="project-card-description">
-            <h4>{params.title}</h4>
-            <p>{params.description}</p>
-            <p>
-              <span>Skills Used: </span>
-              {params.skills}
-            </p>
+    <div className="project-card-padding">
+      <div class="project-card">
+        <a href={params.link} target="_blank" rel="noreferrer">
+          <div
+            className="project-card-img col"
+            style={{
+              background: `url(${params.img})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="project-card-description">
+              <h4>{params.title}</h4>
+              <p>{params.description}</p>
+              <p>
+                <span>Skills Used: </span>
+                {params.skills}
+              </p>
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
+      </div>
     </div>
   );
 };
 
 const Projects = () => {
+
   return (
-    <section id="projects" className="projects container-fluid">
-      <div>
-        <div class="w-100 row">
+    <section id="projects" className="projects">
+      <div class="container-fluid p-0 m-0 ">
+        <div class="row row-cols-lg-3 g-4 justify-content-around">
           <ProjectCard
             img={smartmouth}
             title="Smartmouth"
@@ -58,30 +61,25 @@ const Projects = () => {
             skills="Java, XML, AndroidStudios"
             link="https://github.com/krash3125/Anagrams"
           />
-        </div>
-        <div class="w-100 row mt-4">
-          <div class="col-md-4 mx-auto">
-            <ProjectCard
-              img={chess}
-              title="Chess"
-              description="A Java application developed to play among friends"
-              skills="Java, JSwing"
-              link="https://github.com/krash3125/Chess"
-            />
-          </div>
-          <div class="col-md-4 mx-auto">
-            <ProjectCard
-              img={pcq}
-              title="Private Clinic Queue (In Development)"
-              description="A doctor/patient service in which small clinics can easily manage patients in an organized and quick manner"
-              skills="HTML/CSS, JS, Mongoose, BcryptJS, Passport, JWT, Axois"
-              link="https://github.com/krash3125/pcq3"
-            />
-          </div>
+          <ProjectCard
+            img={chess}
+            title="Chess"
+            description="A Java application developed to play among friends"
+            skills="Java, JSwing"
+            link="https://github.com/krash3125/Chess"
+          />
+          <ProjectCard
+            img={pcq}
+            title="Private Clinic Queue (In Development)"
+            description="A doctor/patient service in which small clinics can easily manage patients in an organized and quick manner"
+            skills="HTML/CSS, JS, Mongoose, BcryptJS, Passport, JWT, Axois"
+            link="https://github.com/krash3125/pcq3"
+          />
         </div>
       </div>
     </section>
   );
 };
+
 
 export default Projects;

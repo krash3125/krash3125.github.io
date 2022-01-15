82961555a4f3d7d135eb72b66789392914b90737
krash3125 <khannakushagr@gmail.com>
2022-01-14 16:10:31 -0800

    gradient

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,21 +3,26 @@ import "../page.css";
 
 const NavBar = () => {
   return (
-    <Navbar color="dark" dark expand fixed="top">
-      <Collapse navbar>
-        <Nav className="mx-auto" navbar>
-          <NavItem className="mx-2">
-            <NavLink href="#landing">Home</NavLink>
-          </NavItem>
-          <NavItem className="mx-2">
-            <NavLink href="#projects">Projects</NavLink>
-          </NavItem>
-          <NavItem className="mx-2">
-            <NavLink href="#about">About</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    // <Navbar color="dark" dark expand fixed="top">
+    //   <Collapse navbar>
+    //     <Nav className="mx-auto" navbar>
+    //       <NavItem className="mx-2">
+    //         <NavLink href="#landing">Home</NavLink>
+    //       </NavItem>
+    //       <NavItem className="mx-2">
+    //         <NavLink href="#projects">Projects</NavLink>
+    //       </NavItem>
+    //       <NavItem className="mx-2">
+    //         <NavLink href="#about">About</NavLink>
+    //       </NavItem>
+    //     </Nav>
+    //   </Collapse>
+    // </Navbar>
+    <nav class="navbar">
+      <a href="#landing">Home</a>
+      <a href="#projects">Projects</a>
+      <a href="#about">About</a>
+    </nav>
   );
 };
 
